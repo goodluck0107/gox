240bb9d38dc89ecabd1a3e859d996fc77b90e17b
xiongtao <251454910@qq.com>
2025-03-12 09:19:16 +0800

    json message

---
+++ handler/message/json.go
@@ -0,0 +1,19 @@
+package message
+
+import "encoding/json"
+
+func Json(v any) IMessage {
+	return &jsonMessage{v: v}
+}
+
+type jsonMessage struct {
+	v any
+}
+
+func (bean *jsonMessage) Decode(e []byte) error {
+	return json.Unmarshal(e, bean.v)
+}
+
+func (bean *jsonMessage) Encode() ([]byte, error) {
+	return json.Marshal(bean.v)
+}
