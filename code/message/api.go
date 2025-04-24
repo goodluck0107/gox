e7abeba2669b0f769ec261c51b1cf81e397a9f9c
xiongtao <251454910@qq.com>
2025-04-24 15:05:37 +0800

    fix

--- code/message/api.go
+++ code/message/api.go
@@ -12,27 +12,27 @@ func Unmarshal(msgType MessageType, b []byte, v any) error {
 	if msgType == MessageTypePB { // ProtoBuffer解码(二进制->消息)
 		pm, ok := v.(proto.Message)
 		if !ok {
-			return errors.New("param is not proto.Message while protoType is ProtoTypePB")
+			return errors.New("param:%v is not proto.Message while protoType is ProtoTypePB")
 		}
 		err := proto.Unmarshal(b, pm)
 		if err != nil {
-			return fmt.Errorf("param can not unmarshal to proto.Message while protoType is ProtoTypePB:%v", err)
+			return fmt.Errorf("param:%v can not unmarshal to proto.Message while protoType is ProtoTypePB:%v", b, err)
 		}
 		return nil
 	} else if msgType == MessageTypeJson { // json解码(二进制->消息)
 		err := json.Unmarshal(b, v)
 		if err != nil {
-			return fmt.Errorf("param can not unmarshal to json while protoType is ProtoTypeJson:%v", err)
+			return fmt.Errorf("param:%v can not unmarshal to json while protoType is ProtoTypeJson:%v", b, err)
 		}
 		return nil
 	} else if msgType == MessageTypeCustom { // 自定义解码(二进制->消息)
 		bm, ok := v.(CustomMessage)
 		if !ok {
-			return fmt.Errorf("param is not message.CustomMessage while protoType is ProtoTypeBN:%v", "not CustomMessage")
+			return fmt.Errorf("param:%v is not message.CustomMessage while protoType is ProtoTypeBN:%v", b, "not CustomMessage")
 		}
 		err := bm.Decode(b)
 		if err != nil {
-			return fmt.Errorf("param can not unmarshal to message.CustomMessage while protoType is ProtoTypeBN:%v", err)
+			return fmt.Errorf("param:%v can not unmarshal to message.CustomMessage while protoType is ProtoTypeBN:%v", b, err)
 		}
 		return nil
 	}
