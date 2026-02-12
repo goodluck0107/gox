88cda13d7636db0557926c49c83942c3d74c1bc3
hexu <hexvckck@gmail.com>
2026-02-12 12:19:02 +0800

    fix

--- executor/EventExecutor.go
+++ executor/EventExecutor.go
@@ -1,6 +1,6 @@
 package executor
 
-var executorInstance *RoutinePool = NewRoutinePool(1000, 4096)
+var executorInstance *RoutinePool = NewRoutinePool(3000, 4096)
 
 func FireEvent(e Event) {
 	executorInstance.FireEvent(e)
